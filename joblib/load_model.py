bcbcb8f10e78526dd973adaf585e9b1795041126
RitheshNM <89503999+RitheshNM@users.noreply.github.com>
2021-10-14 18:59:15 +0530

    Add files via upload

---
+++ joblib/load_model.py
@@ -0,0 +1,11 @@
+import joblib
+
+model= joblib.load('dib_79.pkl')
+
+output= model.predict([[1,2,3,4,5,6,7,8]])
+print(output)
+
+if output[0]==1:
+    print('diabetic')
+else:
+    print('non diabetic')
---
+++ pickle/train.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+url="https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names= ['preg','plas','pres','skin','test','mass','pedi','age','class']
+
+df=pd.read_csv(url)
+df.columns=names
+print(df)
+
+x=df.drop(['class'], axis=1)
+y=df['class']
+print(x)
+
+x_train,x_test,y_train,y_test=model_selection.train_test_split(x,y, test_size=0.2, random_state=101)
+
+model=LogisticRegression()
+model.fit(x_train,y_train)
+
+print(model.score(x_train,y_train))
+print(model.score(x_test,y_test))
+
+pickle.dump(model,open('dib_79.pkl','wb'))
